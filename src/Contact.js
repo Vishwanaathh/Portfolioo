42feea3c2760b1dc7a300e7715fb6e69b8e2e70a
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2024-06-29 21:52:01 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -20,19 +20,19 @@ export default function Contact() {
 
     const stylee = {
      // Conditional opacity based on flag state
-     opacity:flag?1:0,
+     
      
 
      transition:'opacity 2s ease'
     };
     const styleeee={
-        opacity:slide?1:0,
+        
         transition:'opacity 2s ease',
         transform:'translate(1%)'
     }
     const styleee = {
         // Conditional opacity based on flag state
-        opacity:flagg?1:0,
+        
         transition:'opacity 2s ease'
        };
        
@@ -52,7 +52,7 @@ export default function Contact() {
             <div class="box1">
             <h1 id="head">Contact Info</h1>
             <button onClick={toggle}>Click here for toggling Contact info</button>
-          {flagg&&(<ul style={stylee}>
+          {slide&&(<ul style={stylee}>
                 <li class="new">Email: vishwanaathh4@gmail.com</li>
                 <li class="new">City: Hyderabad</li>
             </ul>)}
@@ -60,7 +60,7 @@ export default function Contact() {
             <div class="box1">
                 <h1>Other skills</h1>
             <button onClick={togglee}>Click here for toggling Other Skills</button>
-            {slide&&(<ul style={styleee}>
+            {flagg&&(<ul style={styleee}>
                 <li class="new" ><h3 id="info">Python: I am skilled in building ML and DL Models in python</h3> </li>
                 <li class="new"><h3 id="info">C++: I am skilled in solving DSA problems using C++</h3></li>
                 <li class="new"><h3 id="info">I am skilled in using Javascript and Java to build Web Apps.</h3></li>
