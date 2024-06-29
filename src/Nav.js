269579147fd6e76512b9ba59d0bd00985414f63e
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2024-06-29 19:05:45 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -7,12 +7,12 @@ export default function Nav(){
     }
 
     return(
-        <div id="main"><h1><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Education' id="tagg">Yadavalli Viswanath Parashuram</a></h1>
+        <div id="main"><h1><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/home' id="tagg">Yadavalli Viswanath Parashuram</a></h1>
         <ul id="list">
-            <li><a href='http://localhost:3001/Education'>Education</a></li>
-            <li><a href='http://localhost:3001/Skills'>Skills</a></li>
-            <li><a href='http://localhost:3001/Certifications'>Certifications</a></li>
-            <li><a href='http://localhost:3001/Experience'>Experience</a></li>
+            <li><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Education'>Education</a></li>
+            <li><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Skills'>Skills</a></li>
+            <li><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Certifications'>Certifications</a></li>
+            <li><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Experience'>Experience</a></li>
             <li onClick={toggle}>Projects</li>
 
             </ul>
