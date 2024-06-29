2ed3f8ae15bfac5d61a31d5c3f186a28b46f7b8d
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2024-06-29 19:35:11 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -7,12 +7,12 @@ export default function Nav(){
     }
 
     return(
-        <div id="main"><h1><a href='https://portfolioo-1z7t62id6-vishwanaathhs-projects.vercel.app/' id="tagg">Yadavalli Viswanath Parashuram</a></h1>
+        <div id="main"><h1><a href='https://portfolioo-git-master-vishwanaathhs-projects.vercel.app/' id="tagg">Yadavalli Viswanath Parashuram</a></h1>
         <ul id="list">
-            <li><a href='https://portfolioo-1z7t62id6-vishwanaathhs-projects.vercel.app/Education'>Education</a></li>
-            <li><a href='https://portfolioo-1z7t62id6-vishwanaathhs-projects.vercel.app/Skills'>Skills</a></li>
-            <li><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Certifications'>Certifications</a></li>
-            <li><a href='https://portfolioo-cx0nz822j-vishwanaathhs-projects.vercel.app/Experience'>Experience</a></li>
+            <li><a href='https://portfolioo-git-master-vishwanaathhs-projects.vercel.app/Education'>Education</a></li>
+            <li><a href='https://portfolioo-git-master-vishwanaathhs-projects.vercel.app/Skills'>Skills</a></li>
+            <li><a href='https://portfolioo-git-master-vishwanaathhs-projects.vercel.app/Certifications'>Certifications</a></li>
+            <li><a href='https://portfolioo-git-master-vishwanaathhs-projects.vercel.app/Experience'>Experience</a></li>
             <li onClick={toggle}>Projects</li>
 
             </ul>
