8d78e0495e33faba7eee05922e0d9d8f00cb1bc9
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2024-06-30 21:56:23 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -40,27 +40,27 @@ export default function Contact() {
     return (
         <div animate={{opacity:1}} id="contact">
             <div class="box1">
-                <h1>Interests</h1>
-            <button onClick={toggleee}>Click here to  View my Interests</button>
+                <h1 id="Interests">Interests</h1>
+            <button onClick={toggleee} id="InterestsB">Click here to  View my Interests</button>
              
-            {slide&&(<ul style={styleeee}>
+            {slide&&(<ul style={styleeee} id="first">
                 <li class="new"><h3 id="info">I am interested in building fast,scalable,user friendly web apps </h3></li>
                 <li class="new"><h3 id="info">I can demonstrate skills in management and leadership</h3></li>
                 <li class="new"><h3 id="info">I have a passion for teaching</h3></li>
             </ul>)}
             </div>
             <div class="box1">
-            <h1 id="head">Contact Info</h1>
-            <button onClick={toggle}>Click here to view Contact info</button>
-          {flag&&(<ul style={stylee}>
+            <h1 id="Cinfo">Contact Info</h1>
+            <button onClick={toggle} id="CinfoB">Click here to view Contact info</button>
+          {flag&&(<ul style={stylee} id="second">
                 <li class="new">Email: vishwanaathh4@gmail.com</li>
                 <li class="new">City: Hyderabad</li>
             </ul>)}
             </div>
             <div class="box1">
-                <h1>Other skills</h1>
-            <button onClick={togglee}>Click To View My Other Skills</button>
-            {flagg&&(<ul style={styleee}>
+                <h1 id="Oskills>Other skills</h1>
+            <button onClick={togglee} id="OskillsB">Click To View My Other Skills</button>
+            {flagg&&(<ul style={styleee} id="third">
                 <li class="new" ><h3 id="info">Python: I am skilled in building ML and DL Models in python</h3> </li>
                 <li class="new"><h3 id="info">C++: I am skilled in solving DSA problems using C++</h3></li>
                 <li class="new"><h3 id="info">I am skilled in using Javascript and Java to build Web Apps.</h3></li>
